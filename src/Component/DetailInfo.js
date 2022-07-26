b33563e0d6a8ac828649e8f7a9691bbb63eee615
SumiAkter2 <akteralisha36@gmail.com>
2022-07-26 10:49:31 +0600

    added onclick

--- src/Component/DetailInfo.js
+++ src/Component/DetailInfo.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import { buildStyles, CircularProgressbar, CircularProgressbarWithChildren } from 'react-circular-progressbar';
 // import ProgressBar from "@ramonak/react-progress-bar";
-const DetailInfo = ({ info }) => {
+const DetailInfo = ({ info, handleIncrease }) => {
 
+    const handleDecrease = (id) => {
+        const quantity = info?.stepstarget;
+        console.log('decrease', quantity);
+    }
     return (
 
         <div className='App my-4 rounded-3 border-0'>
@@ -25,7 +29,7 @@ const DetailInfo = ({ info }) => {
                     })} />
                 </div>
                 <div className='d-flex flex-column ps-2 text-center'>
-                    <button className='bg-dark text-light border-0 px-1 rounded-pill'>+</button><span className='fw-bold'>  {info.stepstarget}</span> target  <button className='bg-dark text-light border-0 px-1 rounded-pill'>-</button></div>
+                    <button onClick={() => handleIncrease(info.userId)} className='bg-dark text-light border-0 px-1 rounded-pill' >+</button><span className='fw-bold' >{info.stepstarget} K</span> target  <button onClick={() => handleDecrease(info.userId)} className='bg-dark text-light border-0 px-1 rounded-pill'>-</button></div>
 
                 <div className='ms-5 d-flex flex-column align-items-center justify-content-center'>
                     <p><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" width="20px" className='icon-bg'><path d="M0 256C0 114.6 114.6 0 256 0C397.4 0 512 114.6 512 256C512 397.4 397.4 512 256 512C114.6 512 0 397.4 0 256zM371.8 211.8C382.7 200.9 382.7 183.1 371.8 172.2C360.9 161.3 343.1 161.3 332.2 172.2L224 280.4L179.8 236.2C168.9 225.3 151.1 225.3 140.2 236.2C129.3 247.1 129.3 264.9 140.2 275.8L204.2 339.8C215.1 350.7 232.9 350.7 243.8 339.8L371.8 211.8z" /></svg> {info.performedDate}</p>
--- src/Component/Page1.js
+++ src/Component/Page1.js
@@ -8,23 +8,46 @@ const Page1 = () => {
             .then(res => res.json())
             .then(data => setInfo(data))
     }, [])
+    const handleIncrease = () => {
+        let quantity = infos?.stepstarget;
+        //  quantity++;
+        setInfo(quantity);
+        console.log('increase', quantity);
+    }
     return (
         <div className='text-light container text-center'>
+
             <div className='row'>
-
-
-
                 {
                     infos.map(info => <DetailInfo key={info.userId}
                         info={info}
+                        handleIncrease={handleIncrease}
                     ></DetailInfo>)
                 }
 
             </div>
-            <div className='w-25 '>
-                <ProgressBar completed={50} maxCompleted={70} />
-                <ProgressBar completed={30} maxCompleted={70} />
-                <ProgressBar completed={60} maxCompleted={70} />
+            <div style={{ 'width': '250px' }} className='  rounded p-2 App'>
+                <div style={{ 'fontSize': '10px' }} className='bg-dark p-2 rounded mb-2'>
+                    <div className='d-flex justify-content-between'>
+                        <p>PROTEIN</p>
+                        <p>70%</p>
+                    </div>
+                    <ProgressBar completed={50} maxCompleted={70} />
+                </div>
+                <div style={{ 'fontSize': '10px' }} className='bg-dark p-2 rounded mb-2'>
+                    <div className='d-flex justify-content-between '>
+                        <p >FAT</p>
+                        <p>70%</p>
+                    </div>
+                    <ProgressBar completed={30} maxCompleted={70} />
+                </div>
+                <div style={{ 'fontSize': '10px' }} className='bg-dark p-2 rounded mb-2'>
+                    <div className='d-flex justify-content-between'>
+                        <p>CARBS</p>
+                        <p>70%</p>
+                    </div>
+                    <ProgressBar completed={60} maxCompleted={70} />
+                </div>
             </div>
         </div>
     );
