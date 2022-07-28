0d10bc1fb83309a8a1d24425294b14a1e842150a
SumiAkter2 <akteralisha36@gmail.com>
2022-07-28 09:02:01 +0600

    added design

--- src/Component/Page1.js
+++ src/Component/Page1.js
@@ -1,56 +1,31 @@
-import React, { useEffect, useState } from 'react';
-import DetailInfo from './DetailInfo';
+import React, { useEffect, useState } from "react";
+import DetailInfo from "../Component/DetailInfo";
 import ProgressBar from "@ramonak/react-progress-bar";
 const Page1 = () => {
-    const [infos, setInfo] = useState([]);
-    useEffect(() => {
-        fetch('data.json')
-            .then(res => res.json())
-            .then(data => setInfo(data))
-    }, [])
-    const handleIncrease = () => {
-        let quantity = infos?.stepstarget;
-        //  quantity++;
-        setInfo(quantity);
-        console.log('increase', quantity);
-    }
-    return (
-        <div className='text-light container text-center'>
-
-            <div className='row'>
-                {
-                    infos.map(info => <DetailInfo key={info.userId}
-                        info={info}
-                        handleIncrease={handleIncrease}
-                    ></DetailInfo>)
-                }
-
-            </div>
-            <div style={{ 'width': '250px' }} className='  rounded p-2 App'>
-                <div style={{ 'fontSize': '10px' }} className='bg-dark p-2 rounded mb-2'>
-                    <div className='d-flex justify-content-between'>
-                        <p>PROTEIN</p>
-                        <p>70%</p>
-                    </div>
-                    <ProgressBar completed={50} maxCompleted={70} />
-                </div>
-                <div style={{ 'fontSize': '10px' }} className='bg-dark p-2 rounded mb-2'>
-                    <div className='d-flex justify-content-between '>
-                        <p >FAT</p>
-                        <p>70%</p>
-                    </div>
-                    <ProgressBar completed={30} maxCompleted={70} />
-                </div>
-                <div style={{ 'fontSize': '10px' }} className='bg-dark p-2 rounded mb-2'>
-                    <div className='d-flex justify-content-between'>
-                        <p>CARBS</p>
-                        <p>70%</p>
-                    </div>
-                    <ProgressBar completed={60} maxCompleted={70} />
-                </div>
-            </div>
-        </div>
-    );
+  const [infos, setInfo] = useState([]);
+  useEffect(() => {
+    fetch("data.json")
+      .then((res) => res.json())
+      .then((data) => setInfo(data));
+  }, []);
+  // console.log(infos);
+  return (
+    <div className="text-light container text-center">
+      <div className="row">
+        {infos.map((info) => {
+          // const { stepstarget, userId, name, email } = info;
+          // console.log(stepstarget);
+          return <DetailInfo key={info.userId} info={info}></DetailInfo>;
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default Page1;
+
+/* return (<DetailInfo key={info.userId}
+    info={info}
+    steps={steps}
+    setSteps={setSteps}
+></DetailInfo>) */
